bc2dcc15e333c244cde201547f770eedfd3d2eec
Lucas Balestero <Lucas Balestero>
2016-11-30 15:40:41 -0300

    Human Class creation

---
+++ human.js
@@ -0,0 +1,9 @@
+class Human{
+	constructor(dateOfBirth){
+		this.dateOfBirth = dateofBirth;
+		this.lifeStatus = this.lifeStatus;
+		this.intelligence = this.intelligence;
+		this.strength = this.strength;
+		this.deathChance = this.deathChance;
+	}
+}
