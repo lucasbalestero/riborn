1554c88bb0f5711314bb8e08d5c66b33f9bd2f27
Lucas Balestero <Lucas Balestero>
2016-11-30 14:27:07 -0300

    Fixing indentation issues

--- main.js
+++ main.js
@@ -70,60 +70,63 @@ function updateAge(){
 }
 
 function convertDate(inputFormat) {
-  function pad(s) { return (s < 10) ? '0' + s : s; } // Add a 0 to the left when number is less than 10
-  var d = new Date(inputFormat);
-  return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/');
+	function pad(s) { return (s < 10) ? '0' + s : s; } // Add a 0 to the left when number is less than 10
+	var d = new Date(inputFormat);
+	return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/');
 }
 
 function getAge(firstDate,finalDate){
 
-		var startDate = new Date(firstDate);
-		var endDate = new Date(finalDate);
-    var year = endDate.getFullYear() - startDate.getFullYear();
+	var startDate = new Date(firstDate);
+	var endDate = new Date(finalDate);
+	var year = endDate.getFullYear() - startDate.getFullYear();
 
-    if((endDate.getMonth() + 1) >= (startDate.getMonth() + 1)){
-        var month = (endDate.getMonth() + 1)-(startDate.getMonth() + 1);
-    }else{
-        var month = 12 + (endDate.getMonth() + 1)-(startDate.getMonth() + 1);
-        year--;
-    }
+	if((endDate.getMonth() + 1) >= (startDate.getMonth() + 1)){
+		var month = (endDate.getMonth() + 1)-(startDate.getMonth() + 1);
+	}else{
+		var month = 12 + (endDate.getMonth() + 1)-(startDate.getMonth() + 1);
+		year--;
+	}
 
-    if(endDate.getDate() >= startDate.getDate()){
-        var day = endDate.getDate()-startDate.getDate();
-    }else{
-        var day = 31 + endDate.getDate()-startDate.getDate();
-        month--;
+	if(endDate.getDate() >= startDate.getDate()){
+		var day = endDate.getDate()-startDate.getDate();
+	}else{
+		var day = 31 + endDate.getDate()-startDate.getDate();
+		month--;
 
-        if(month < 0){
-            month = 11;
-            year--;
-        }
-    }
-		var fmtYear;
-		var fmtMonth;
-		var fmtDay;
-		if(year == 1){
-			fmtYear = "Year";
-		}else{
-			fmtYear = "Years";
+		if(month < 0){
+			month = 11;
+			year--;
 		}
-		if(month == 1){
-			fmtMonth = "Month";
-		}else{
-			fmtMonth = "Months";
-		}
-		if(day == 1){
-			fmtDay = "Day";
-		}else{
-			fmtDay = "Days";
-		}
-		
-		var formatedAge = day + " " + fmtDay;
-		if (month > 0){
-				formatedAge = month + " " + fmtMonth + " " + formatedAge;
-		}
-		if (year > 0){
-				formatedAge = year + " " + fmtYear + " " + formatedAge;
-		}
-    return formatedAge;
+	}
+
+	var fmtYear;
+	var fmtMonth;
+	var fmtDay;
+	if(year == 1){
+		fmtYear = "Year";
+	}else{
+		fmtYear = "Years";
+	}
+
+	if(month == 1){
+		fmtMonth = "Month";
+	}else{
+		fmtMonth = "Months";
+	}
+
+	if(day == 1){
+		fmtDay = "Day";
+	}else{
+		fmtDay = "Days";
+	}
+
+	var formatedAge = day + " " + fmtDay;
+	if (month > 0){
+		formatedAge = month + " " + fmtMonth + " " + formatedAge;
+	}
+	if (year > 0){
+		formatedAge = year + " " + fmtYear + " " + formatedAge;
+	}
+	return formatedAge;
 }
